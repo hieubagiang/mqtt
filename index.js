4980ffca86ea8f86c7b40b1a12874a880bfaf324
Pham Doan Hieu <hieupd@rikkeisoft.com>
2021-09-29 15:38:04 +0700

    dual port ws and mqtt

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const aedes = require("aedes")();
-const httpServer = require("http").createServer();
-const ws = require("websocket-stream");
-const port = process.env.PORT || 1883;
+const aedes = require('aedes');
+const port = 1883
+const server = require('net').createServer(aedes.handle);
+server.listen(port, function() {
+    console.log('Ades MQTT listening on port: ' + port)
+})
 
-ws.createServer({ server: httpServer }, aedes.handle);
-
-httpServer.listen(port, () => {
-  console.log("websocket server listening on port ", port);
+const wsPort = 8883
+const httpServer = require('http').createServer()
+const ws = require('websocket-stream')
+ws.createServer({ server: httpServer }, aedes.handle)
+httpServer.listen(wsPort, function () {
+    console.log('Aedes MQTT-WS listening on port: ' + wsPort)
 });
